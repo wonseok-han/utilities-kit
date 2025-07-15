f1ccb01b5aee1e263fcba92dd29f9c81bd2ff351
wonseok-han <oshan1112@gmail.com>
2025-07-16 00:45:29 +0900

    vercel analytics 추가

--- apps/dev-kit/src/app/layout.tsx
+++ apps/dev-kit/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 
+import { Analytics } from '@vercel/analytics/next';
 import localFont from 'next/font/local';
 import './globals.css';
 
@@ -36,6 +37,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );

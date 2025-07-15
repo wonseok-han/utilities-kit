945b3dddbfc332fd7a543466d36d0098bb3c6f7b
wonseok-han <oshan1112@gmail.com>
2025-07-15 19:14:39 +0900

    svgr 기본 설정 추가

--- apps/dev-tools/next.config.js
+++ apps/dev-tools/next.config.js
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      oneOf: [
+        {
+          issuer: /\.[jt]sx?$/, // .jsx, .tsx 파일에서 SVG를 React 컴포넌트로 사용
+          use: ['@svgr/webpack'],
+        },
+        {
+          issuer: /\.s[0,1]css$/, // .css, .scss 파일에서 SVG를 URL로 사용
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[hash].[ext]',
+                outputPath: 'static/media',
+                publicPath: '/_next/static/media',
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
